de5f3b02486e183fec4f72acc55ca81c9c71da39
Yuxiao Wang <yx99.wang@gmail.com>
2021-02-17 17:58:50 -0500

    fizzbuzz

--- fizzbuzz.cpp
+++ fizzbuzz.cpp
@@ -6,11 +6,20 @@ using namespace std;
 
 int main(int argc, char** argv)
 {
-   int X, Y, N;
-   cin >> X;
-   cin >> Y;
-   cin >> N;
+    cout << "enter X, Y, N:" << endl;
+    int X, Y, N;
+    cin >> X;
+    cin >> Y;
+    cin >> N;
 
-   // Your code here
+    for (int i = 1; i <= N; ++i) {
+        string s;
+        if (i % X == 0) s += "Fizz";
+        if (i % Y == 0) s += "Buzz";
+        if (s.empty())  s = to_string(i);
+        cout << s << endl;
+    }
+
+    return 0;
 }
 
