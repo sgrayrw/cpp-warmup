4a5b0a72683b41606b2ef884375d673a999eedeb
Yuxiao Wang <yx99.wang@gmail.com>
2021-02-17 23:22:02 -0500

    done

--- circle.cpp
+++ circle.cpp
@@ -1,4 +1,35 @@
 #include "circle.h"
 
-// your code here
+Circle::Circle() : radius(0) {}
 
+Circle::~Circle() = default;
+
+Circle::Circle(const Circle& other) : radius(other.radius) {}
+
+Circle::Circle(float radius) : radius(radius) {}
+
+Circle& Circle::operator=(const Circle& other) {
+    radius = other.radius;
+    return *this;
+}
+
+Circle Circle::operator+(const Circle& other) {
+    return Circle(radius + other.radius);
+}
+
+std::ostream& operator<<(std::ostream& os, const Circle& circle) {
+    os << circle.radius;
+    return os;
+}
+
+float Circle::getRadius() const {
+    return radius;
+}
+
+void Circle::setRadius(float radius) {
+    this->radius = radius;
+}
+
+float Circle::computeArea() const {
+    return M_PI * radius * radius;
+}
--- circle.h
+++ circle.h
@@ -1,8 +1,24 @@
-// Name:
-// Date:
+// Name: Yuxiao Wang
+// Date: 02/17/2021
 
 #pragma once
 #include <iostream>
 
-// your code here
+class Circle {
+public:
+    Circle();
+    virtual ~Circle();
+    Circle(const Circle& other);
+    explicit Circle(float radius);
 
+    Circle& operator=(const Circle& other);
+    Circle operator+(const Circle& other);
+    friend std::ostream& operator<<(std::ostream& os, const Circle& circle);
+
+    float getRadius() const;
+    void setRadius(float radius);
+    float computeArea() const;
+
+private:
+    float radius;
+};
